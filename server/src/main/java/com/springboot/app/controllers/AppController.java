940a5e598d27cbb9257e03393ea9e94c0932fa23
Konda Yaramala <reachme.ykreddy@gmail.com>
2017-09-30 18:40:42 -0500

    Using optionals sample start

--- server/src/main/java/com/springboot/app/controllers/AppController.java
+++ server/src/main/java/com/springboot/app/controllers/AppController.java
@@ -1,17 +1,22 @@
 package com.springboot.app.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/")
 public class AppController {
 
-
+	// JAVA 8 Optional has many feature one of then is the optional.map and alse
+	// possible to return in null cases or throw an error, if it doesn't met with
+	// the criteria
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getCustomerBillTo() {
-		return "this is response";
+		Optional<String> response = Optional.of("Sample Response");
+		return response.map(String::toUpperCase).orElse("Error resnponse");
 	}
 
 }
