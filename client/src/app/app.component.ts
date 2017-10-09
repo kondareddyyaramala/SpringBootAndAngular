ab780b1abe8e724a0327e5145cb5ec4a5f860c78
Konda Yaramala <reachme.ykreddy@gmail.com>
2017-10-08 22:24:17 -0500

    Using hostlistener to listen the document click

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick($event) {
+    console.log('On Document click', $event);
+  }
+
 }
